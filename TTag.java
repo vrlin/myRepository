96e1f31c941a155f9d5f496e097c0a7cf9bf7b24
vrlin <73688111+vrlin@users.noreply.github.com>
2020-10-30 16:16:33 +0800

    Update TTag.java

--- TTag.java
+++ TTag.java
@@ -9,38 +9,5 @@ public class TTag implements Serializable {
 
     private String name;
 
-    public TTag(Integer id, Integer pid, String name) {
-        this.id = id;
-        this.pid = pid;
-        this.name = name;
-    }
-
-    public TTag() {
-    }
-
-    private static final long serialVersionUID = 1L;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Integer getPid() {
-        return pid;
-    }
-
-    public void setPid(Integer pid) {
-        this.pid = pid;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name == null ? null : name.trim();
-    }
+   
 }
